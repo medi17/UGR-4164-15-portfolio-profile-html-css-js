d8f1b8afaba12a57bbe83c7c653529cf2e0e5e5d
Medan <medanabebe17@gmail.com>
2024-12-27 21:03:56 +0300

    Updated home

--- src/JavaScript/home.js
+++ src/JavaScript/home.js
@@ -1,6 +1,6 @@
 const menu = document.getElementById('menu');
 const navbar = document.getElementById('navbar');
-const menu_bar = document.getElementById('menu_bar')
+const menu_bar = document.getElementById('menu_bar');
 menu.addEventListener('click', function () {
     logo.style.display = 'none';
     menu.style.display = 'none';
@@ -9,3 +9,16 @@ menu.addEventListener('click', function () {
     navbar.style.backgroundColor = 'var(--gray-50)';
 
 });
+
+const downloadButton = document.getElementById('download-CV');
+
+downloadButton.addEventListener('click', () => {
+  const link = document.createElement('a');
+  link.href = '../images/addis.jfif'; // Replace with the actual path to your CV file
+  link.download = 'Medan-cv.pdf'; // Set the desired filename for the downloaded file
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+});
+
+
